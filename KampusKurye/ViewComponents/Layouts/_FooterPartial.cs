d1bbc204b3276634a55dc8d4c2247b860cce501b
Hasim-Kartoglu <hasim.kartoglu@stu.gedik.edu.tr>
2025-09-17 17:02:17 +0300

    Add project files.

---
+++ KampusKurye/ViewComponents/Layouts/_FooterPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KampusKurye.ViewComponents.Layouts
+{
+    public class _FooterPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ KampusKurye/ViewComponents/Layouts/_SalesPromotionPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KampusKurye.ViewComponents.Layouts
+{
+    public class _SalesPromotionPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
